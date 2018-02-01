97dc507249ee9f43b056925c6b07b72c21586969
Yuki Sumida <y.sumida.0201@gmail.com>
2018-02-01 22:58:55 +0900

    カメラロールから写真を選択する

--- PhotoLibraryAccess/ViewController.swift
+++ PhotoLibraryAccess/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapSelectPhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
 }
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let _ = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            print("didFinishPickingMediaWithInfo")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
 

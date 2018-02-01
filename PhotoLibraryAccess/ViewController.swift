2e152509bd3326ee712cb0e75bb65454ade59c19
Yuki Sumida <y.sumida.0201@gmail.com>
2018-02-01 22:44:13 +0900

    Create project.

---
+++ PhotoLibraryAccess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoLibraryAccess
+//
+//  Created by Yuki Sumida on 2018/02/01.
+//  Copyright © 2018年 Yuki Sumida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
